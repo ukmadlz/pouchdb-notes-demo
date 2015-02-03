c72fb57cd9ddc138f00f1ebe28895c849d65cbd4
Mike Elsmore <mike@elsmore.me>
2015-02-03 22:20:06 +0100

    The base HTML for the app

---
+++ app.js
@@ -0,0 +1,42 @@
+/***
+  App.js - The actual workings of the demo
+***/
+
+/*
+  Cloudant details
+ */
+var cloudant = {
+  account: "ACCOUNTNAME",
+  apikey: "APIKEY",
+  apipass: "APIPASS"
+}
+
+/*
+  Grab the DB name based on fragment
+ */
+var dbname = window.location.hash.substring(1);
+
+/*
+  Initialise PouchDB
+ */
+
+/*
+  Save a doc
+ */
+
+/*
+  List docs
+ */
+
+/*
+  Read doc
+ */
+
+/*
+  Create DB on Cloudant
+  -- For WebApps *don't* forget to allow CORS
+ */
+
+/*
+  Sync to from DB
+ */
